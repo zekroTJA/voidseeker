c0629d62a37022f6041b4098d4c357ff676576fb
zekroTJA <riho@live.de>
2020-06-04 01:01:30 +0200

    update backend startup and add Dockerfile

--- RESTAPI/Program.cs
+++ RESTAPI/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.IO;
 
 namespace RESTAPI
 {
@@ -10,11 +13,42 @@ namespace RESTAPI
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+        //public static IHostBuilder CreateHostBuilder(string[] args) =>
+        //    Host.CreateDefaultBuilder(args)
+        //        .ConfigureWebHostDefaults(webBuilder =>
+        //        {
+        //            webBuilder.UseStartup<Startup>();
+        //        });
+
+        public static IWebHostBuilder CreateHostBuilder(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true)
+                .AddJsonFile("appsettings.Development.json", optional: true)
+                .AddEnvironmentVariables(prefix: "VS_")
+                .Build();
+
+            return new WebHostBuilder()
+                .UseKestrel()
+                .UseConfiguration(config)
+                .UseUrls(config.GetValue<string>("WebServer:URL") ?? "http://localhost:80")
+                .UseDefaultServiceProvider((context, options) =>
                 {
-                    webBuilder.UseStartup<Startup>();
-                });
+                    options.ValidateScopes = context.HostingEnvironment.IsDevelopment();
+                })
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging
+                        .AddConfiguration(hostingContext.Configuration.GetSection("Logging"))
+                        .AddConsole(c =>
+                        {
+                            c.TimestampFormat = "yyyy/MM/dd - HH:mm:ss | ";
+                        })
+                        .AddDebug()
+                        .AddEventSourceLogger();
+                })
+                .UseStartup<Startup>();
+        }
     }
 }
