4f25aa812f0f89ac5da8fc1b1c05df4dcc35de3b
zekroTJA <riho@live.de>
2020-06-05 19:46:48 +0200

    add imagescount to get user endpoint

---
+++ RESTAPI/Models/Responses/UserDetailsModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace RESTAPI.Models.Responses
+{
+    public class UserDetailsModel : UserModel
+    {
+        [JsonPropertyName("imagescount")]
+        public long ImagesCount { get; set; }
+
+        public UserDetailsModel()
+        {
+        }
+
+        public UserDetailsModel(UserModel user) : base(user)
+        {
+        }
+    }
+}
