8781b8e09ccbe207572569a6b77eab68784b465b
zekroTJA <riho@live.de>
2020-06-05 11:49:10 +0200

    add file selector to upload route

--- WebApp/src/routes/upload/upload.tsx
+++ WebApp/src/routes/upload/upload.tsx
@@ -14,13 +14,19 @@ class UploadRoute extends Component<UploadRouteProps> {
     image: {} as ImageModel,
     tagSuggestions: (null as any) as string[],
     dragging: false,
-    dropText: 'Drop your image here',
+    dropText: 'Drop your image here or click to select a file.',
   };
 
   public render() {
     return (
       <div>
         <h2>Image Upload</h2>
+        <input
+          type="file"
+          id="upload-fileinput"
+          style={{ display: 'none' }}
+          onChange={this.onFileInputChange.bind(this)}
+        />
         <div
           className={`upload-drop-zone${
             this.state.dragging ? ' upload-drop-zone-over' : ''
@@ -28,6 +34,7 @@ class UploadRoute extends Component<UploadRouteProps> {
           onDragOver={this.onDragOver.bind(this)}
           onDragLeave={this.onDragLeave.bind(this)}
           onDrop={this.onDrop.bind(this)}
+          onClick={this.onClick.bind(this)}
         >
           <p>{this.state.dropText}</p>
         </div>
@@ -49,12 +56,28 @@ class UploadRoute extends Component<UploadRouteProps> {
   private async onDrop(e: React.DragEvent<HTMLDivElement>) {
     e.stopPropagation();
     e.preventDefault();
-    this.setState({ dragging: false, dropText: 'Uploading image...' });
 
     const files = e.dataTransfer.files;
     if (files.length <= 0) return;
 
     const file = files[0];
+    await this.uploadFile(file);
+  }
+
+  private onClick() {
+    const fileInput = document.getElementById('upload-fileinput');
+    if (!fileInput) return;
+    fileInput.click();
+  }
+
+  private async onFileInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const file = e.target.files ? e.target.files[0] : null;
+    if (!file) return;
+    await this.uploadFile(file);
+  }
+
+  private async uploadFile(file: File) {
+    this.setState({ dragging: false, dropText: 'Uploading image...' });
     try {
       const res = await RestAPI.uploadImage(file);
       this.props.history.replace(`/images/${res.uid}/edit`);
