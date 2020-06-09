f3835bf32e955e0a2b67a5848308487bb23aa4e5
zekroTJA <riho@live.de>
2020-06-09 09:53:24 +0200

    add dark theme stylesheet [#11]

--- WebApp/src/index.tsx
+++ WebApp/src/index.tsx
@@ -3,9 +3,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import LocalStorage from './util/localstorage';
 
 import './index.scss';
 import './vars.css';
+
+if (LocalStorage.get<boolean>('dark_theme')) {
+  import('./apply-darktheme');
+}
 
 ReactDOM.render(
   <React.StrictMode>
